39254695c1c332668a26b12f7dc2659c425082e5
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2025-09-26 19:32:15 +0700

    Implement SFBSwapchainSwap*

--- SimFramebuffer/SimFramebuffer.m
+++ SimFramebuffer/SimFramebuffer.m
@@ -7,8 +7,24 @@
 
 @import CoreGraphics;
 @import Foundation;
+@import IOSurface;
+#import "IOMobileFramebuffer.h"
 
 const uint64_t simFramebufferGetStructName = 0xa9017bfda9be4ff4;
+
+#pragma pack(push,1)
+typedef struct {
+    uint32_t mode_id;       // 0x00 = 1
+    uint32_t width;         // 0x04 = 1206 (maybe width in pixels)
+    uint32_t height;        // 0x08 = 2622 (maybe height in pixels)
+    uint32_t scale;         // 0x0c = 3 (scale factor?)
+    uint32_t unknown0; //refresh;       // 0x10 = 1 (Hz? or just flag)
+    char     pixel_format[4]; // 0x14 = "ARGB"
+    uint32_t unknown1; //color_depth;   // 0x18 = 1 (bits/component?)
+    uint32_t timing;        // 0x1c = 6000 (maybe Hz*100? 60.00 Hz)
+    uint8_t  flags[3];      // 0x20 = 01 01 01 (boolean flags)
+} SFBDisplayMode;
+#pragma pack(pop)
 
 @interface SFBConnection : NSObject
 @property(nonatomic) NSString *name;
@@ -19,8 +35,11 @@ const uint64_t simFramebufferGetStructName = 0xa9017bfda9be4ff4;
 @property(nonatomic) NSString *displayUID;
 @property(nonatomic) NSString *name;
 @property(nonatomic) CGSize size;
+@property(nonatomic) IOMobileFramebufferRef fbConn;
+@property(nonatomic) IOSurfaceRef surface;
 @end
-@interface SFBSwapchain : NSObject
+@interface SFBSwapchain : SFBDisplay
+// For convenience, we make SFBSwapchain a subclass of SFBDisplay
 @end
 
 @implementation SFBConnection
@@ -40,16 +59,19 @@ const uint64_t simFramebufferGetStructName = 0xa9017bfda9be4ff4;
     self.type = 0;
     self.displayUID = @"PurpleMain";
     self.name = @"LCD";
-    self.size = CGSizeMake(1179, 2556); // TODO: get actual size
+    IOMobileFramebufferGetMainDisplay(&_fbConn);
+    assert(_fbConn);
+    IOMobileFramebufferGetDisplaySize(_fbConn, &_size);
     return self;
 }
 @end
 
-@implementation SFBSwapchain
-@end
-
 SFBConnection *SFBConnectionCreate(id x0, NSString *name) {
-    return [[SFBConnection alloc] initWithName:name];
+    static SFBConnection *sharedConn = nil;
+    if(!sharedConn) {
+        sharedConn = [[SFBConnection alloc] initWithName:name];
+    }
+    return sharedConn;
 }
 BOOL SFBConnectionConnect(SFBConnection *conn, NSError **error) {
     return YES;
@@ -70,8 +92,20 @@ NSString *SFBDisplayGetDisplayUID(SFBDisplay *display) {
 NSString *SFBDisplayGetName(SFBDisplay *display) {
     return display.name;
 }
-SFBDisplay *SFBDisplayGetCurrentMode(SFBDisplay *display) {
-    return display;
+void SFBDisplayGetCurrentMode(SFBDisplay *display, SFBDisplayMode *mode) {
+    assert(display);
+    assert(mode);
+    mode->mode_id = 1;
+    mode->width = display.size.width;
+    mode->height = display.size.height;
+    mode->scale = 3;
+    mode->unknown0 = 1;
+    memcpy(mode->pixel_format, "ARGB", 4);
+    mode->unknown1 = 1;
+    mode->timing = 6000; // 60.00 Hz
+    mode->flags[0] = 1;
+    mode->flags[1] = 1;
+    mode->flags[2] = 1;
 }
 CGSize SFBDisplayGetDeviceSize(SFBDisplay *mode) {
     return mode.size;
@@ -80,19 +114,33 @@ CGSize SFBDisplayGetCurrentCanvasSize(SFBDisplay *mode) {
     return mode.size;
 }
 SFBSwapchain *SFBDisplayCreateSwapchain(SFBDisplay *display, CGSize size, int x1_scaleMaybe, int x2, NSError **error) {
-    return [SFBSwapchain new];
+    return (id)display;
 }
 
+// here we borrow IOSurfaceSetValue
 BOOL SFBSwapchainSwapBegin(SFBSwapchain *swapchain, NSUInteger time, NSError **error) {
-    // TODO: implement
+//    int token;
+//    return IOMobileFramebufferSwapBegin(swapchain.fbConn, &token) == 0;
+    //IOSurfaceSetValue(@"SwapCmd", @(1));
     return YES;
 }
 BOOL SFBSwapchainSwapAddSurface(SFBSwapchain *swapchain, IOSurfaceRef surface, CGRect rect1, CGRect rect2,  NSUInteger x2, NSUInteger x3, NSError **error) {
-    // TODO: implement
+//    return IOMobileFramebufferSwapSetLayer(swapchain.fbConn, 0, surface, rect1, rect2, 0) == 0;
+    // do note that both rect1 and rect2 are the same
+    // for optimization we pack them into a single 48-bit integer
+    swapchain.surface = surface;
+    uint64_t packedFrame = ((uint64_t)rect1.origin.x << 36) | ((uint64_t)rect1.origin.y << 24) | ((uint64_t)rect1.size.width << 12) | ((uint64_t)rect1.size.height & 0xfff);
+    IOSurfaceSetValue(surface, (__bridge CFTypeRef _Nonnull)@"SwapCmd", (__bridge CFTypeRef _Nonnull)@(packedFrame));
     return YES;
 }
 BOOL SFBSwapchainSwapSubmit(SFBSwapchain *swapchain, NSError **error) {
+//    return IOMobileFramebufferSwapEnd(swapchain.display.fbConn) == 0;
+    IOSurfaceSetValue(swapchain.surface, (__bridge CFTypeRef _Nonnull)@"SwapCmd", @(0));
     return YES;
+}
+BOOL SFBSwapchainSwapSetCallback(SFBSwapchain *swapchain, void *context, dispatch_queue_t queue, void (^callback)(NSError *error, SFBSwapchain *swapchain, uint64_t arg1, uint64_t arg2, void *context)) {
+    // I'm not really sure what this callback is for, so let's just pretend like this function failed
+    return NO;
 }
 
 // stubs
@@ -145,7 +193,6 @@ void SFBSwapchainGetPixelFormat() { abort(); }
 void SFBSwapchainGetPresentationMode() { abort(); }
 void SFBSwapchainGetTypeID() { abort(); }
 void SFBSwapchainSwapCancel() { abort(); }
-void SFBSwapchainSwapSetCallback() { abort(); }
 
 /*
  OBJC_CLASS_$__SFBSwapSurfaceFence
---
+++ SimRenderServer/hook.x
@@ -0,0 +1,35 @@
+@import IOSurface;
+#import "IOMobileFramebuffer.h"
+
+typedef CFTypeRef IOSurfaceClientRef;
+int IOSurfaceClientGetID(IOSurfaceClientRef client);
+void IOSurfaceClientSetValue(IOSurfaceClientRef client, NSString *key, id value);
+
+IOMobileFramebufferRef fbConn;
+%hookf(void, IOSurfaceClientSetValue, IOSurfaceClientRef client, NSString *key, id value) {
+    if([key isEqual:@"SwapCmd"]) {
+        uint64_t val = (uint64_t)[value unsignedLongLongValue];
+        if(val == 0) { // SFBSwapchainSwapSubmit
+            int token;
+            IOMobileFramebufferSwapEnd(fbConn);
+            IOMobileFramebufferSwapBegin(fbConn, &token);
+        } else {
+            static int surfaceID = 0;
+            static IOSurfaceRef surface;
+            int surfaceIDCurr = IOSurfaceClientGetID(client);
+            if(surfaceID != surfaceIDCurr) {
+                surfaceID = surfaceIDCurr;
+                surface = IOSurfaceLookup(surfaceIDCurr);
+            }
+            CGRect frame = CGRectMake(val >> 36, (val >> 24) & 0xFFF, (val >> 12) & 0xFFF, val & 0xFFF);
+            IOMobileFramebufferSwapSetLayer(fbConn, 0, surface, frame, frame, 0);
+        }
+        return;
+    }
+    %orig(client, key, value);
+}
+
+%ctor {
+    IOMobileFramebufferGetMainDisplay(&fbConn);
+    assert(fbConn);
+}
