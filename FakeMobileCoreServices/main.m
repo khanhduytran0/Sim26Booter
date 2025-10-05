720b4bd676b764f07058be40e94a3c0cd4e81331
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2025-10-05 10:32:42 +0700

    Fix cfpfefsd crashes, allow invalid code

--- FakeMobileCoreServices/main.m
+++ FakeMobileCoreServices/main.m
@@ -27,7 +27,31 @@ void handleFaultyTextPage(int signum, struct __siginfo *siginfo, void *context) 
     });
 }
 
+#define CS_DEBUGGED 0x10000000
+int csops(pid_t pid, unsigned int ops, void *useraddr, size_t usersize);
+int fork();
+int ptrace(int, int, int, int);
+int isJITEnabled() {
+    int flags;
+    csops(getpid(), 0, &flags, sizeof(flags));
+    return (flags & CS_DEBUGGED) != 0;
+}
+
 __attribute__((constructor)) static void MobileCoreServicesInit() {
+    // enable JIT
+    if (!isJITEnabled()) {
+        // Enable JIT
+        int pid = fork();
+        if (pid == 0) {
+            ptrace(0, 0, 0, 0);
+            exit(0);
+        } else if (pid > 0) {
+            while (wait(NULL) > 0) {
+                usleep(1000);
+            }
+        }
+    }
+    
     // catch SIGILL
     struct sigaction sigAction;
     sigAction.sa_sigaction = handleFaultyTextPage;
